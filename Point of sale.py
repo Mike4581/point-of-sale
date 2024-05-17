9606529342e4282b9f66d30ad3107e4807b6712e
Mike4581 <michaeljordaan40@gmail.com>
2024-05-17 14:21:34 +0200

    Update Point of sale.py

--- Point of sale.py
+++ Point of sale.py
@@ -1,63 +1,44 @@
-cheese =50.00
-milk =30.00
-bread =15.00
-price =0.00
+total =0.00 #declare viarables
+output = ""
 
-print("cheese: Php R50.00")
-print("milk: Php R30.00")
-print("bread: Php R15.00")
-
-while True:
-    choice=input('\nChoose an item: cheese, milk, bread\n')
-    if choice == 'cheese':
-        choice=input('Would you like to pick another product? y/n\n')
-        if choice == 'y':
-            choice=input('\nChoose an product: cheese, milk, bread\n')
-        else:
-            for cost in price:
-                sum += cost
-                break
-                print("Total cost: Php",sum)
-                print(" ")
+def addCheese(): #functions
+    return 50.00
     
-    elif choice == 'milk':
-        choice=input('Would you like to pick another product? y/n\n')
-        if choice == 'y':
-            choice=input('\nChoose an product: cheese, milk, bread\n')
-        else:
-            for cost in price:
-                sum += cost
-                break
-                print("Total cost: Php",sum)
-                print(" ")
 
-    elif choice == 'bread':
-        choice=input('Would you like to pick another product? y/n\n')
-        if choice == 'y':
-            choice=input('\nChoose an product: cheese, milk, bread\n')
-        else:
-            for cost in price:
-                sum += cost
-                break
-                print("Total cost: Php",sum)
-                print(" ")
+def addBread():
+    return 15.00
+    
 
+def addMilk():
+    return 30.00
+    
+    
+mylist =["1. cheese: R50.00","2. milk: R30.00","3. bread: R15.00"] #declare list
+
+done = False #Sentinel viarable
+
+while done == False: 
+    
+
+    for x in mylist: #for loop 
+        print(x) #display list
+    
+    choice=input('\nEnter a item number (Enter 0 to check out):  \n') #get user input
+
+
+    if input ==1:                       
+        total += addCheese()                #increase total
+        output += "cheese: R50.00\n"
+    elif input ==2:
+        total += addMilk()
+        output += "milk: R30.00\n"
+    elif input ==3:
+         total += addBread()
+         output += "bread: R15.00\n"
     else:
-        print("Error!")
-        break
-#create our dictionary, shopping products as keys and price of product as value
-shoppingDict = {"cheese":50.00,"milk":30.00,"bread":15.00}
-#iterate through the dictionaries products printing them out in a certain format
-for k,v in shoppingDict.products():print(f'{k}: Php {v}')
-while True:
-    #promp user to enter an product
-    choice=input('\nChoose an product: cheese, milk, bread\n')
-    #if so add the key's value(price)to price
-    try:price += shoppingDict.get(choice)
-    #if the user does not enter a valid product, we print a error
-    except KeyError:print('Error')
-    #ask them if they want to try again
-    if input('Would you like to try again? y/n') == 'n':
-        print(f'Total Cost: {price}')
-        break
-    
+        done = True
+
+print(str(output))      #display total
+print("_____________")
+
+print("total: " + str(total))
